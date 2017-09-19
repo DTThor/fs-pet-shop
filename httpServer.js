084cd6f60776571c4f154a109991967baba2283a
dtthor <d.t.thorwaldson@gmail.com>
2017-09-19 15:39:13 -0500

    complete part 2

--- httpServer.js
+++ httpServer.js
@@ -1,21 +1,24 @@
 const http = require('http');
 const fs = require('fs');
 const server = http.createServer((req, res)=>{
-  console.log('server is running');
   //route GET
-  if(req.url.slice(1) === 'pets'){
+  if(req.url.slice(1).includes('pets')){
+    let url = req.url.slice(1).split('/');
     fs.readFile('./pets.json', 'utf8', (err, data) => {
-    let myData = JSON.stringify(data);
-    while (myData.includes('\\')){
-      myData = myData .replace('\\', '')
-    }
-    res.end(myData);
-  });
-
+      let myData = data;
+      if(url.length === 2){
+        myData = JSON.parse(data);
+        myData = myData[url[1]];
+        myData = JSON.stringify(myData);
+        res.setHeader('Content-Type', 'application/json');
+      };
+      if(url.length > 2 || !myData){
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+      }
+      res.end(myData);
+    });
   }
-
-//   console.log(reqInfo, req.headers['user-agent']);
-  //res.end(myData)
 });
 
 server.listen(8000);
