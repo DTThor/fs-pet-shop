2c58e613eacaa20095ef7a489da53f42433f646f
dtthor <d.t.thorwaldson@gmail.com>
2017-09-22 15:28:35 -0500

    Getting post to work on part 4

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -5,27 +5,13 @@ const petsPath = path.join(__dirname, 'pets.json')
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
-const bodyParser = require('body-parser')
-
-// create application/json parser
-var jsonParser = bodyParser.json()
-
-// create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 
-// // POST /pets gets JSON bodies
-// app.post('/pets', petsPath, function (req, res) {
-//   fs.writeFile(petsPath, req.body, function(err, petsJSON){
-//     if (!req.body){
-//     return res.sendStatus(400)
-//     }
-//   // create user in req.body
-//   req.body = 'yo';
-//   res.send(req.body)
-//   })
-// })
-
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(morgan('short'));
 app.disable('x-powered-by');
 
 app.get('/pets', function(req, res){
@@ -55,6 +41,36 @@ app.get('/pets/:id', function(req, res){
     res.send(pets[id]);
   })
 })
+
+app.post('/pets', function(req, res, next){
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+    if(err){
+      return next(err);
+    }
+    const pets = JSON.parse(petsJSON);
+    const age = Number.parseInt(req.body.age);
+    const kind = req.body.kind;
+    const name = req.body.name;
+    if (Number.isNaN(age) || !kind || !name){
+      return res.sendStatus(400);
+    }
+    const pet = { age, kind, name };
+    pets.push(pet);
+    const newPets = JSON.stringify(pets);
+    fs.writeFile(petsPath, newPets, function(err){
+      if(err){
+        return next(err);
+      }
+      res.send(pet);
+    });
+  });
+});
+
+
+
+
+
+
 
 app.use(function(req, res){
   res.sendStatus(404);
