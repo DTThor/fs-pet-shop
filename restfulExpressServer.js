650f43a4086c6dca32bec42595a960629a16ff76
dtthor <d.t.thorwaldson@gmail.com>
2017-09-22 16:03:37 -0500

    Completing patch and delete. CRUD complete

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -47,16 +47,16 @@ app.post('/pets', function(req, res, next){
     if(err){
       return next(err);
     }
-    const pets = JSON.parse(petsJSON);
-    const age = Number.parseInt(req.body.age);
-    const kind = req.body.kind;
-    const name = req.body.name;
+    let pets = JSON.parse(petsJSON);
+    let age = Number.parseInt(req.body.age);
+    let kind = req.body.kind;
+    let name = req.body.name;
     if (Number.isNaN(age) || !kind || !name){
       return res.sendStatus(400);
     }
-    const pet = { age, kind, name };
+    let pet = { age, kind, name };
     pets.push(pet);
-    const newPets = JSON.stringify(pets);
+    let newPets = JSON.stringify(pets);
     fs.writeFile(petsPath, newPets, function(err){
       if(err){
         return next(err);
@@ -66,8 +66,57 @@ app.post('/pets', function(req, res, next){
   });
 });
 
+app.patch('/pets/:id', function(req, res, next) {
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON){
+    if (err) {
+      return next(err);
+    }
+    let id = Number.parseInt(req.params.id);
+    let pets = JSON.parse(petsJSON);
+    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+    let age = Number.parseInt(req.body.age);
+    let { kind, name } = req.body;
+    if (!Number.isNaN(age)) {
+      pets[id].age = age;
+    }
+    if (kind) {
+      pets[id].kind = kind;
+    }
+    if (name) {
+      pets[id].name = name;
+    }
+    let newPets = JSON.stringify(pets);
+    fs.writeFile(petsPath, newPets, function(err){
+      if (err) {
+        return next(err);
+      }
+      res.send(pets[id]);
+    });
+  });
+});
 
-
+app.delete('/pets/:id', function(req, res, next){
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON){
+    if(err){
+      return next(err);
+    }
+    let id = Number.parseInt(req.params.id);
+    let pets = JSON.parse(petsJSON);
+    if(id < 0 || id >= pets.length || Number.isNaN(id)){
+      return res.sendStatus(404);
+    }
+    let pet = pets.splice(id, 1)[0];
+    let newPets = JSON.stringify(pets);
+    fs.writeFile(petsPath, newPets, function(err){
+      if(err){
+        return next(err);
+      }
+      res.send(pet);
+    })
+  })
+})
 
 
 
